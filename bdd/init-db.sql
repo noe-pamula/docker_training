165c7d1ae510c473456349c3d50a330ada9672f2
Noe <noe.pamula@davidson.fr>
2020-12-04 10:54:33 +0100

    docker_in_docker

--- bdd/init-db.sql
+++
@@ -1,7 +0,0 @@
-CREATE DATABASE workshop;
-
-USE workshop;
-
-CREATE TABLE USERS (id INT PRIMARY KEY NOT NULL,name VARCHAR(100),firstname VARCHAR(100));
-
-INSERT INTO USERS VALUES (1, "anne", "onyme");
